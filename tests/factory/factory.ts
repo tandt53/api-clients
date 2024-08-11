ea4bd5b6d372848ca0f13d13163a09ac24fb65bd
tan <dothetan.040490@gmail.com>
2024-08-11 15:09:54 +0700

    update factory

--- tests/factory/factory.ts
+++ tests/factory/factory.ts
@@ -1,30 +1,31 @@
-import {faker} from "@faker-js/faker";
-import {Pet} from "../models/Pet";
+import { faker } from "@faker-js/faker";
+import { Pet } from "../models/Pet";
 
 export const newUser = {
-    "id": faker.datatype.number(),
-    "email": faker.internet.email().toLowerCase(),
-    "firstName": faker.name.firstName(),
-    "lastName": faker.name.lastName(),
-    "password": faker.internet.password(),
-    "phone": faker.phone.number(),
-    "username": faker.name.firstName().toLowerCase()
+  id: faker.number.int(),
+  email: faker.internet.email().toLowerCase(),
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  password: faker.internet.password(),
+  phone: faker.phone.number(),
+  username: faker.person.firstName().toLowerCase(),
 };
 
-
 export const newPet: Pet = {
-    "id": faker.datatype.number(),
-    "category": {
-        "id": 1,
-        "name": "Dogs"
+  id: faker.number.int(),
+  category: {
+    id: 1,
+    name: "Dogs",
+  },
+  name: faker.person.firstName(),
+  photoUrls: [
+    "https://img.freepik.com/free-photo/puppy-dog-glass-isolated-white-background_1232-1677.jpg",
+  ],
+  tags: [
+    {
+      id: 1,
+      name: "Friendly",
     },
-    "name": faker.name.firstName(),
-    "photoUrls": ["https://img.freepik.com/free-photo/puppy-dog-glass-isolated-white-background_1232-1677.jpg"],
-    "tags": [
-        {
-            "id": 1,
-            "name": "Friendly"
-        }
-    ],
-    "status": Pet.status.AVAILABLE
-}
+  ],
+  status: Pet.status.AVAILABLE,
+};
